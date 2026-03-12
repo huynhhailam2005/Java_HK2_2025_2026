5628c1820c2fbf29fc7af81cc79d53372e58ea5d
Huỳnh Lâm <hailam.coc@gmail.com>
2026-03-12 14:46:00 +0700

    Initial commit: Setup Spring Boot base project with PostgreSQL

---
+++ src/main/java/com/example/srpm/SrpmApplication.java
@@ -0,0 +1,13 @@
+package com.example.srpm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrpmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrpmApplication.class, args);
+	}
+
+}
