ded952dcf9df229d137622e9e8b24ac2b2998cfe
Ben170105 <minhtamdao2005@gmail.com>
2026-03-15 00:20:30 +0700

    [FE] feat: thiết kế giao diện trang login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,15 @@
-import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
 
 function App() {
-    const [count, setCount] = useState(0)
-
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-            <h1 className="text-4xl font-bold text-blue-600 mb-6">
-                Test Tailwind CSS v4
-            </h1>
-
-            <div className="bg-white p-8 rounded-xl shadow-md text-center">
-                <p className="text-gray-700 mb-4">Click thử nút bên dưới xem sao nhé Leader!</p>
-                <button
-                    className="px-6 py-2 bg-blue-500 text-white font-semibold rounded-lg hover:bg-blue-600 transition duration-300"
-                    onClick={() => setCount((count) => count + 1)}
-                >
-                    Count is {count}
-                </button>
-            </div>
-        </div>
-    )
+        <BrowserRouter>
+            <Routes>
+                {/* Tạm thời cài đặt đường dẫn mặc định (/) trỏ thẳng vào trang Đăng nhập */}
+                <Route path="/" element={<LoginPage />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
-export default App
+export default App;
